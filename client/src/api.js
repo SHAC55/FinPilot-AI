32960263f6c9a40a8030adc431d172fadc117ba6
SAIF CHAUDHARY <SAIFCHOUDHARY.0666@GMAIL.COM>
2026-04-01 18:31:39 +0530

    Fixed API 1.2

--- client/src/api.js
+++ client/src/api.js
@@ -20,36 +20,6 @@ API.interceptors.request.use(
   (error) => Promise.reject(error),
 );
 
-// =======================
-// AUTH APIs
-// =======================
-export const AuthAPI = {
-  login: (data) => API.post("/auth/login", data),
-  register: (data) => API.post("/auth/register", data),
-};
 
-// =======================
-// 💳 CREDIT APIs
-// =======================
-export const CreditAPI = {
-  markCompleted: (id) => API.patch(`/transaction/credit/${id}`),
-  getCompleted: () => API.get(`/transaction/getcompletedcredit`),
-};
-
-// =======================
-// 💸 DEBIT APIs
-// =======================
-export const DebitAPI = {
-  markCompleted: (id) => API.patch(`/transaction/debit/${id}`),
-  getCompleted: () => API.get(`/transaction/getcompletedebit`),
-};
-
-// =======================
-// 🧾 BILL APIs
-// =======================
-export const BillAPI = {
-  markCompleted: (id) => API.patch(`/bill/${id}`),
-  getCompleted: () => API.get(`/bill/completedbills`),
-};
 
 export default API;
