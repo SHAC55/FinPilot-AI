056548e8bc84470df07655ff8de725cab158f568
SAIF CHAUDHARY <SAIFCHOUDHARY.0666@GMAIL.COM>
2026-04-01 12:39:30 +0530

    Fixed 1.0 nodemailer

--- server/utils/emailService.js
+++ server/utils/emailService.js
@@ -5,6 +5,7 @@ dotenv.config();
 
 export const transporter = nodemailer.createTransport({
   service: "gmail",
+    secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
