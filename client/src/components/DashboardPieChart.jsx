8b48bd70c3c407b816c1e540d60aaac9094af402
SAIF CHAUDHARY <SAIFCHOUDHARY.0666@GMAIL.COM>
2026-04-01 22:59:31 +0530

    Fixed appcontext 1.0

--- client/src/components/DashboardPieChart.jsx
+++ client/src/components/DashboardPieChart.jsx
@@ -169,7 +169,7 @@ const DashboardPieChart = () => {
       .sort((a, b) => b.value - a.value);
   }, [filteredExpenses]);
 
-   = data.reduce((sum, d) => sum + d.value, 0);
+   const total= data.reduce((sum, d) => sum + d.value, 0);
 
   const CustomTooltip = ({ active, payload }) => {
     if (active && payload && payload.length) {
