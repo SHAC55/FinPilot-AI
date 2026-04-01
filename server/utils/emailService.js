8432635aa62b2063c5e9e937713dd5322d8c26e7
SAIF CHAUDHARY <SAIFCHOUDHARY.0666@GMAIL.COM>
2026-04-01 19:19:43 +0530

    Fixed SMTP

--- server/utils/emailService.js
+++ server/utils/emailService.js
@@ -1,29 +1,30 @@
 import nodemailer from "nodemailer";
 import dotenv from "dotenv";
 
-dotenv.config(); 
+dotenv.config();
 
 export const transporter = nodemailer.createTransport({
   service: "gmail",
-    secure: false,
+  secure: false,
+  port: 465,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
 });
 
-export const sendEmail = async ({ to, subject, text,html }) => {
+export const sendEmail = async ({ to, subject, text, html }) => {
   try {
     await transporter.sendMail({
       from: process.env.EMAIL_USER,
-      to, 
+      to,
       subject,
       text,
-      html
+      html,
     });
     console.log(`Email sent to ${to}: ${subject}`);
   } catch (err) {
     console.error("Error sending email:", err);
-    throw err; 
+    throw err;
   }
 };
