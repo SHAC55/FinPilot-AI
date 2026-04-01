53eb08cb44a8a7eb06f95158ee0142143efff4dc
SAIF CHAUDHARY <SAIFCHOUDHARY.0666@GMAIL.COM>
2026-04-01 19:34:32 +0530

    FIXED SMTP 1.2

--- server/utils/emailService.js
+++ server/utils/emailService.js
@@ -5,8 +5,9 @@ dotenv.config();
 
 export const transporter = nodemailer.createTransport({
   service: "gmail",
+  port: 587,
   secure: false,
-  port: 465,
+  requireTLS: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
